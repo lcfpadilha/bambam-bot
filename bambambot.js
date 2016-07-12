60e0fc002d7000b2f051c32bfba530fb091b39c9
Leonardo Padilha <lcfpadilha@gmail.com>
2016-07-12 02:03:48 -0300

    ajustando regex

--- bambambot.js
+++ bambambot.js
@@ -2,23 +2,13 @@ var TelegramBot = require('node-telegram-bot-api');
 var http = require('http');
 
 var token = '220901453:AAHnwqAOTcbwBW8zQRwUyxs1CA5I-Y6OLdA';
-
-// Setup polling way
 var bot = new TelegramBot(token, {polling: true});
 
-// Matches /echo [whatever]
-bot.onText(/(.*[^1-9]+)13([^1-9]+.*)/, function (msg, match) {
+// Encontra textos com 13
+bot.onText(/(^13$)|(([^0-9]+)13([^0-9]+))|(^13([^0-9]+))|(([^0-9]+)13$)/, function (msg, match) {
   var fromId = msg.from.id;
   var resp = "É TREZE MEMO PORRA!";
   bot.sendMessage(fromId, resp);
-});
-
-// Any kind of message
-bot.on('message', function (msg) {
-  var chatId = msg.chat.id;
-  // photo can be: a file path, a stream or a Telegram file_id
-  var photo = 'cats.png';
-  bot.sendPhoto(chatId, photo, {caption: 'Lovely kittens'});
 });
 
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
@@ -32,5 +22,5 @@ var server = http.createServer(function(request, response) {
 });
 
 server.listen( port, ipaddress, function() {
-    console.log((new Date()) + ' Server is listening on port 8080');
+    console.log('BOORA CUMPADI! SAI DE CASA, SERVI PRA CARALHO NA PORTA 8080!');
 });
